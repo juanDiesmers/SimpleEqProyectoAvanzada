e2c20808cbc02d6ccc14575e2721ae1536a6c608
amers_gos <juandiegoapal_@outlook.es>
2022-05-14 19:21:26 -0500

    Added Parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,6 +53,9 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParametersLayout();
+    juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParametersLayout() };
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
